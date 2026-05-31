e8dc76268672f081c692f88d022262f5eaa870d6
EXEFile_7f3c9d8a <169616430+EXEFile7f3c9d8a@users.noreply.github.com>
2026-05-30 21:11:50 -0400

    character test

--- src/main/java/Debug/VernMap/MapDebug.java
+++ src/main/java/Debug/VernMap/MapDebug.java
@@ -10,7 +10,7 @@ public class MapDebug {
         System.out.println("0".hashCode());
         System.out.println(obj);
         obj.addTag("Value", null,
-                new Object[]{"VrenCoreTest_1234567890_ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz_~!@#$%^&*()_+{}|:<>?-=[]\\\\;',./\""});
+                new Object[]{"VrenCoreTest_1234567890_ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz_~!@#$%^&*()_+{}|:<>?-=[]\\\\;',./\"ΩДאم中あ가कአ\ud83d\ude80"});
         System.out.println(obj.getTag(obj));
     }
 }
