aae97c1d6202dca4155622a79767a4ea302dd472
EXEFile_7f3c9d8a <exefile.7f3c9d8a@gmail.com>
2026-06-07 10:30:54 -0400

    Hotfix
    fixing the problem that jitpack cant build this on their server causes not able to use it

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("This is not runnable file, please goto:");
+        System.out.println("https://github.com/EXEFile7f3c9d8a/Vren/tree/MapRelease");
+        System.out.println("https://exefile7f3c9d8a.github.io/Vren/docs/VrenMapFormatDoc/VrenMapFormatDoc.html");
+        System.out.println("There will be docs about this, i am lazy so itz only in cmd haha");
+    }
+}
